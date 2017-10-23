ce09f878c3b72b45f445165056629b673c3f6afa
Swinkels <david.swinkels@wur.nl>
2017-10-23 15:54:37 +0200

    Added Rstudio Added more required libraries for R-packages Added QGIS, git gui, Google Earth, Postgis

--- InstallScriptGeoscripting.sh
+++ InstallScriptGeoscripting.sh
@@ -11,16 +11,44 @@ sudo apt-get install qgis spatialite-gui spatialite-bin gdal-bin
 # DM: Install R from the CRAN repository
 # add manual change in sources.list
 
-sudo apt-get install r-base r-base-dev rkward spyder
+sudo apt-get install r-base r-base-dev rkward spyder gdebi-core
+wget https://download1.rstudio.org/rstudio-xenial-1.1.383-amd64.deb
+sudo gdebi -n rrstudio-xenial-1.1.383-amd64.deb
+rm rstudio-xenial-1.1.383-amd64
 # NOTE: RKWard is built against the default R packages. It needs to be installed from a PPA for the linked-to-CRAN version.
+# NOTE2: RStudio is installed from rstudio repository
+
 
 # Install requirements for packages not part of the CRAN distribution
-sudo apt-get install libgdal-dev
+sudo apt-get install liblibgdal-dev libgeos-dev libxml2-dev libcurl4-openssl-dev libssl-dev
 # Source-install "raster", "googleVis", "lubridate", "leaflet".
 # Make sure to use a directory common to RStudio and RKWard: RKWard settings
 # ~/R/x86_64-pc-linux-gnu-library/3.2
 sudo apt-get install r-cran-spatstat r-cran-jsonlite r-cran-zoo r-cran-magrittr r-cran-stringr
 sudo apt-get install r-cran-colorspace r-cran-yaml r-cran-digest r-cran-rcpp r-cran-mime r-cran-dichromat r-cran-plyr r-cran-munsell r-cran-labeling r-cran-base64enc r-cran-rcolorbrewer r-cran-scales r-cran-sp
+
+# Install other software: QGIS, gitGUI, google earth, Postgis, Postgresql
+sudo sh -c 'echo "deb http://qgis.org/debian xenial main" >> /etc/apt/sources.list'  
+sudo sh -c 'echo "deb-src http://qgis.org/debian xenial main " >> /etc/apt/sources.list'  
+wget -O - http://qgis.org/downloads/qgis-2017.gpg.key | gpg --import
+gpg --fingerprint CAEB3DC3BDF7FB45
+gpg --export --armor CAEB3DC3BDF7FB45 | sudo apt-key add -
+sudo apt-get update && sudo apt-get install qgis python-qgis  
+sudo apt-get install git-gui
+cd /tmp
+mkdir google-earth && cd google-earth
+wget http://archive.ubuntu.com/ubuntu/pool/main/l/lsb/lsb-invalid-mta_4.1+Debian11ubuntu8_all.deb
+wget http://archive.ubuntu.com/ubuntu/pool/main/l/lsb/lsb-security_4.1+Debian11ubuntu8_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/main/l/lsb/lsb-core_4.1+Debian11ubuntu8_amd64.deb
+sudo dpkg -i *.deb
+sudo apt -f install
+wget https://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
+sudo dpkg -i google-earth-stable*.deb
+cd
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt xenial-pgdg main" >> /etc/apt/sources.list'
+wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install postgresql-9.5-postgis-2.2 pgadmin3 postgresql-contrib-9.5 postgresql-9.5-postgis-2.2-scripts postgresql-server-dev-9.5 libpq-dev
 
 # Remove rsyslog, already have journald
 sudo systemctl disable rsyslog
