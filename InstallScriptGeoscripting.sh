71cf28b89a37a251c0e0e189ee9a6e86a3592a81
Masiliunas, Dainius <dainius.masiliunas@wur.nl>
2017-10-24 16:41:40 +0200

    Use add-apt-repository, pushd/popd; more comments

--- InstallScriptGeoscripting.sh
+++ InstallScriptGeoscripting.sh
@@ -8,7 +8,7 @@ sudo add-apt-repository ppa:rkward-devel/rkward-stable-cran
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install sshfs mesa-utils manpages firefox xarchiver spyder gdebi-core
-sudo apt-get install qgis spatialite-gui spatialite-bin gdal-bin
+sudo apt-get install spatialite-gui spatialite-bin gdal-bin git-gui
 
 # DM: Install R from the CRAN repository
 sudo apt-get install r-base r-base-dev rkward
@@ -31,25 +31,23 @@ sudo apt-get install libgdal-dev libgeos-dev libproj-dev libxml2-dev libcurl4-op
 sudo apt-get install r-cran-spatstat r-cran-jsonlite r-cran-zoo r-cran-magrittr r-cran-stringr
 sudo apt-get install r-cran-colorspace r-cran-yaml r-cran-digest r-cran-rcpp r-cran-mime r-cran-dichromat r-cran-plyr r-cran-munsell r-cran-labeling r-cran-base64enc r-cran-rcolorbrewer r-cran-scales r-cran-sp
 
-# Install other software: QGIS, gitGUI, google earth, Postgis, Postgresql
-sudo sh -c 'echo "deb http://qgis.org/debian xenial main" >> /etc/apt/sources.list'  
-sudo sh -c 'echo "deb-src http://qgis.org/debian xenial main " >> /etc/apt/sources.list'  
+# QGIS
+sudo add-apt-repository http://qgis.org/debian
 wget -O - http://qgis.org/downloads/qgis-2017.gpg.key | gpg --import
 gpg --fingerprint CAEB3DC3BDF7FB45
 gpg --export --armor CAEB3DC3BDF7FB45 | sudo apt-key add -
 sudo apt-get update && sudo apt-get install qgis python-qgis  
-sudo apt-get install git-gui
-cd /tmp
+
+# Google Earth
+pushd /tmp
 mkdir google-earth && cd google-earth
-wget http://archive.ubuntu.com/ubuntu/pool/main/l/lsb/lsb-invalid-mta_4.1+Debian11ubuntu8_all.deb
-wget http://archive.ubuntu.com/ubuntu/pool/main/l/lsb/lsb-security_4.1+Debian11ubuntu8_amd64.deb
-wget http://archive.ubuntu.com/ubuntu/pool/main/l/lsb/lsb-core_4.1+Debian11ubuntu8_amd64.deb
-sudo dpkg -i *.deb
-sudo apt -f install
+sudo apt-get install lsb-core lsb-base lsb-invalid-mta
 wget https://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
 sudo dpkg -i google-earth-stable*.deb
-cd
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt xenial-pgdg main" >> /etc/apt/sources.list'
+popd
+
+# PostGIS
+sudo add-apt-repository http://apt.postgresql.org/pub/repos/apt
 wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install postgresql-9.5-postgis-2.2 pgadmin3 postgresql-contrib-9.5 postgresql-9.5-postgis-2.2-scripts postgresql-server-dev-9.5 libpq-dev
@@ -58,5 +56,7 @@ sudo apt-get install postgresql-9.5-postgis-2.2 pgadmin3 postgresql-contrib-9.5 
 sudo systemctl disable rsyslog
 sudo systemctl stop rsyslog syslog.socket
 sudo apt-get purge --auto-remove rsyslog
+
+sudo apt autoremove
 
 # Settings: Mousepad should have View -> Color Scheme set
