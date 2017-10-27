c288993c724a1132d4a34423082411f7d9cf3fda
Masiliunas, Dainius <dainius.masiliunas@wur.nl>
2017-10-27 14:31:17 +0200

    Also add ggplot2 for the IntroToVector

--- InstallScriptGeoscripting.sh
+++ InstallScriptGeoscripting.sh
@@ -50,7 +50,7 @@ sudo apt-get install libgdal-dev libgeos-dev libproj-dev libxml2-dev libcurl4-op
 # Source-install "raster", "googleVis", "lubridate", "leaflet".
 # Make sure to use a directory common to RStudio and RKWard: RKWard settings
 # ~/R/x86_64-pc-linux-gnu-library/3.2
-sudo apt install r-cran-spatstat r-cran-jsonlite r-cran-zoo r-cran-magrittr r-cran-stringr
+sudo apt install r-cran-spatstat r-cran-jsonlite r-cran-zoo r-cran-magrittr r-cran-stringr r-cran-ggplot2
 sudo apt install r-cran-colorspace r-cran-yaml r-cran-digest r-cran-rcpp r-cran-mime r-cran-dichromat r-cran-plyr r-cran-munsell r-cran-labeling r-cran-base64enc r-cran-rcolorbrewer r-cran-scales r-cran-sp
 
 # QGIS
