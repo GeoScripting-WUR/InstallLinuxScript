9b230eac7c04734164368f84951916ff7721e219
Masiliunas, Dainius <dainius.masiliunas@wur.nl>
2018-08-22 11:02:05 +0200

    Make the script automated
    More or less; it will still prompt to press enter to confirm adding keys. Perhaps that can also be automated...

--- InstallScriptGeoscripting.sh
+++ InstallScriptGeoscripting.sh
@@ -45,11 +45,11 @@ sudo apt update
 sudo apt upgrade
 
 # DS: Install software
-sudo apt install sshfs mesa-utils manpages firefox gdebi-core curl
-sudo apt install spatialite-gui spatialite-bin gdal-bin git-gui qgis python-qgis
+sudo apt install -y sshfs mesa-utils manpages firefox gdebi-core curl
+sudo apt install -y spatialite-gui spatialite-bin gdal-bin git-gui qgis python-qgis
 
 # DS: Install Grass and hold version to 7.0.3 
-sudo apt install grass=7.0.3-1build1 grass-core=7.0.3-1build1 grass-doc=7.0.3-1build1 grass-gui=7.0.3-1build1
+sudo apt install -y grass=7.0.3-1build1 grass-core=7.0.3-1build1 grass-doc=7.0.3-1build1 grass-gui=7.0.3-1build1
 sudo apt-mark hold grass grass-gui grass-doc grass-core
 
 # DM: Add Git GUI into the menu
@@ -59,7 +59,7 @@ sudo cp git-gui.desktop /usr/local/share/applications/
 # DM: Install R from the CRAN repository and RKWard
 sudo add-apt-repository "deb http://cloud.r-project.org/bin/linux/ubuntu/ xenial/"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
-sudo apt update && sudo apt install r-base r-base-dev rkward
+sudo apt update && sudo apt install -y r-base r-base-dev rkward
 
 # RStudio installation
 RSTUDIO_VERSION="1.1.456"
@@ -70,12 +70,12 @@ rm rstudio-xenial-${RSTUDIO_VERSION}-amd64.deb
 popd
 
 # Install requirements for packages not part of the CRAN distribution
-sudo apt install libgdal-dev libgeos-dev libproj-dev libxml2-dev libcurl4-openssl-dev libssl-dev libudunits2-dev liblwgeom-dev
+sudo apt install -y libgdal-dev libgeos-dev libproj-dev libxml2-dev libcurl4-openssl-dev libssl-dev libudunits2-dev liblwgeom-dev
 # Source-install "raster", "googleVis", "lubridate", "leaflet".
 # Make sure to use a directory common to RStudio and RKWard: RKWard settings
 # ~/R/x86_64-pc-linux-gnu-library/3.2
-sudo apt install r-cran-spatstat r-cran-jsonlite r-cran-zoo r-cran-magrittr r-cran-stringr r-cran-ggplot2
-sudo apt install r-cran-colorspace r-cran-yaml r-cran-digest r-cran-rcpp r-cran-mime r-cran-dichromat r-cran-plyr r-cran-munsell r-cran-labeling r-cran-base64enc r-cran-rcolorbrewer r-cran-scales r-cran-sp
+sudo apt install -y r-cran-spatstat r-cran-jsonlite r-cran-zoo r-cran-magrittr r-cran-stringr r-cran-ggplot2
+sudo apt install -y r-cran-colorspace r-cran-yaml r-cran-digest r-cran-rcpp r-cran-mime r-cran-dichromat r-cran-plyr r-cran-munsell r-cran-labeling r-cran-base64enc r-cran-rcolorbrewer r-cran-scales r-cran-sp
 
 # QGIS
 # DM: NOTE: Check if the key changes in 2018!
@@ -95,7 +95,7 @@ sudo apt install r-cran-colorspace r-cran-yaml r-cran-digest r-cran-rcpp r-cran-
 sudo add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt xenial-pgdg main"
 sudo apt-key adv --keyserver http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc --recv-keys ACCC4CF8
 sudo apt update
-sudo apt install postgresql-9.5-postgis-2.2 pgadmin3 postgresql-contrib-9.5 postgresql-9.5-postgis-2.2-scripts postgresql-server-dev-9.5 libpq-dev
+sudo apt install -y postgresql-9.5-postgis-2.2 pgadmin3 postgresql-contrib-9.5 postgresql-9.5-postgis-2.2-scripts postgresql-server-dev-9.5 libpq-dev
 # DM: Create a user and a database with these credentials
 PGUSER="geoscripting"
 export PGPASSWORD="geoscripting"
