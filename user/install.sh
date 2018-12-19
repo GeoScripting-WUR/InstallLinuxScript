070056c994f6b8a4e7d0077ae686ca64aa093edb
Dainius Masiliūnas <pastas4@gmail.com>
2018-12-19 22:02:55 +0100

    Make conda unattended

--- user/install.sh
+++ user/install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Script to install user-specific tools (i.e. conda)
-sh install-conda.sh
+bash install-conda.sh
 
 # Make a conda environment with all needed tools
-conda create --name geoscripting python=3.6 geopandas spyder owslib pip jupyter
+conda create -y --name geoscripting python=3.6 geopandas spyder owslib pip jupyter
 conda activate geoscripting
-conda install --channel conda-forge folium matplotlib geopy osmnx rasterio geopandas rasterstats owslib pysal descartes
+conda install -y --channel conda-forge folium matplotlib geopy osmnx rasterio geopandas rasterstats owslib pysal descartes
 
 # Install IRKernel for Jupyter
 echo 'install.packages("IRkernel", repos="https://cloud.r-project.org"); IRkernel::installspec()' | R --vanilla
