2736b026dee8dc6b9c156b446558997da9272f50
Dainius Masiliūnas <pastas4@gmail.com>
2018-12-19 14:42:51 +0200

    Add required packages for Python raster/vector lessons

--- user/install.sh
+++ user/install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 # Script to install user-specific tools (i.e. conda)
 sh install-conda.sh
+
+# Make a conda environment with all needed tools
+conda create --name geoscripting python=3.6 geopandas spyder owslib pip jupyter
+conda activate geoscripting
+conda install --channel conda-forge folium matplotlib geopy osmnx rasterio geopandas rasterstats owslib pysal descartes
