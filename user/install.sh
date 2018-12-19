f0432cfa9010be9528c58e48515bcc11a53a370d
Dainius Masiliūnas <pastas4@gmail.com>
2018-12-19 22:13:07 +0100

    Use source activate
    The conda method requires changing the profile instead;
    keep these in sync if we need to change it in the future

--- user/install.sh
+++ user/install.sh
@@ -4,7 +4,7 @@ bash install-conda.sh
 
 # Make a conda environment with all needed tools
 conda create -y --name geoscripting python=3.6 geopandas spyder owslib pip jupyter
-conda activate geoscripting
+source activate geoscripting
 conda install -y --channel conda-forge folium matplotlib geopy osmnx rasterio geopandas rasterstats owslib pysal descartes
 
 # Install IRKernel for Jupyter
