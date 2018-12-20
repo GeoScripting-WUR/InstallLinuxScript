926edc57297ef47bfea6cec9c46a867e16bbacaf
Dainius Masiliūnas <pastas4@gmail.com>
2018-12-20 08:30:18 +0100

    Add seaborn and twython

--- user/install.sh
+++ user/install.sh
@@ -3,9 +3,9 @@
 bash install-conda.sh
 
 # Make a conda environment with all needed tools
-conda create -y --name geoscripting python=3.6 geopandas spyder owslib pip jupyter
+conda create -y --name geoscripting python=3.6 geopandas spyder owslib pip jupyter seaborn
 source activate geoscripting
-conda install -y --channel conda-forge folium matplotlib geopy osmnx rasterio geopandas rasterstats owslib pysal descartes
+conda install -y --channel conda-forge folium matplotlib geopy osmnx rasterio geopandas rasterstats owslib pysal descartes twython
 
 # Install IRKernel for Jupyter
 echo 'install.packages("IRkernel", repos="https://cloud.r-project.org"); IRkernel::installspec()' | R --vanilla
