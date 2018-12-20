0907257719cbc02767129804e665665a25445e22
Dainius Masiliūnas <pastas4@gmail.com>
2018-12-20 10:06:52 +0100

    Add nbgrader to user script

--- user/install.sh
+++ user/install.sh
@@ -5,7 +5,13 @@ bash install-conda.sh
 # Make a conda environment with all needed tools
 conda create -y --name geoscripting python=3.6 spyder pip jupyter seaborn "poppler<0.62"
 source activate geoscripting
-conda install -y --channel conda-forge folium matplotlib geopy osmnx rasterio geopandas rasterstats owslib pysal descartes twython
+conda install -y --channel conda-forge folium matplotlib geopy osmnx rasterio geopandas rasterstats owslib pysal descartes \
+  twython nbgrader
 
 # Install IRKernel for Jupyter
 echo 'install.packages("IRkernel", repos="https://cloud.r-project.org"); IRkernel::installspec()' | R --vanilla
+
+# Activate nbgrader
+jupyter nbextension install --sys-prefix --py nbgrader --overwrite
+jupyter nbextension enable --sys-prefix --py nbgrader
+jupyter serverextension enable --sys-prefix --py nbgrader
