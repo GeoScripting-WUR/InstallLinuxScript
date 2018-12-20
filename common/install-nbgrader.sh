6b458b20c861917a665212113b902cb7120248a0
Dainius Masiliūnas <pastas4@gmail.com>
2018-12-20 09:37:03 +0100

    Fix permission issues with nbgrader config

--- common/install-nbgrader.sh
+++ common/install-nbgrader.sh
@@ -4,5 +4,5 @@
 sudo mkdir -p /srv/nbgrader/exchange
 sudo chmod ugo+rw /srv/nbgrader/exchange
 sudo mkdir -p /etc/jupyter
-sudo echo 'c = get_config()' >> /etc/jupyter/nbgrader_config.py
-sudo echo 'c.Exchange.course_id = "geoscripting"' >> /etc/jupyter/nbgrader_config.py
+echo 'c = get_config()' | sudo tee -a /etc/jupyter/nbgrader_config.py
+echo 'c.Exchange.course_id = "geoscripting"' | sudo tee -a /etc/jupyter/nbgrader_config.py
