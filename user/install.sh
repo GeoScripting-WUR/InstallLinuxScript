12610dd05696d62535c579ff18b719209ccfee77
Dainius Masiliūnas <pastas4@gmail.com>
2019-01-21 15:15:53 +0100

    Try to fix the all-in-one conda environment

--- user/install.sh
+++ user/install.sh
@@ -4,12 +4,13 @@ bash install-conda.sh
 export PATH="$HOME/miniconda3/bin:$PATH"
 
 # Make a conda environment with all needed tools
-conda create -y --name geoscripting python=3.6 spyder pip jupyter seaborn "poppler<0.62"
+conda create -y --name geoscripting python=3.6 spyder pip jupyter seaborn owslib "poppler<0.62"
 source activate geoscripting
-conda install -y --channel conda-forge folium matplotlib geopy osmnx rasterio geopandas rasterstats owslib pysal descartes \
+conda install -y --channel conda-forge folium matplotlib geopy osmnx rasterio geopandas rasterstats pysal descartes \
   twython nbgrader
 # Fix breakage from https://github.com/conda-forge/rasterio-feedstock/issues/98
 conda install -y --channel conda-forge --override-channels "gdal>2.2.4"
+conda install kealib=1.4.7 -y
 
 # Install IRKernel for Jupyter
 echo 'UserLib=Sys.getenv("R_LIBS_USER"); dir.create(UserLib, recursive=TRUE); install.packages("IRkernel", lib=UserLib, repos="https://cloud.r-project.org"); library(IRkernel, lib=UserLib); IRkernel::installspec()' | R --vanilla
