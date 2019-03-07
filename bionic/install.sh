11c427786d040b7188178a40f15c89ecee0494b0
Dainius Masiliūnas <pastas4@gmail.com>
2019-03-07 15:32:23 +0100

    Simplify R and PostGIS installs
    Lots of workarounds no longer needed

--- bionic/install.sh
+++ bionic/install.sh
@@ -15,28 +15,20 @@ sudo apt update
 sudo apt upgrade -y
 
 # DS: Install software
-sudo apt install -y mesa-utils gdebi-core curl spatialite-gui spatialite-bin gdal-bin git-gui qgis python-qgis grass
+sudo apt install -y mesa-utils gdebi-core curl spatialite-gui spatialite-bin gdal-bin git-gui qgis grass
 
 # DM: Add Git GUI into the menu
 sudo mkdir /usr/local/share/applications/
 sudo cp ../common/git-gui.desktop /usr/local/share/applications/
 
-# DM: Install R from the CRAN repository and RKWard
-#sudo add-apt-repository "deb http://cloud.r-project.org/bin/linux/ubuntu/ xenial-cran35/"
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
-#sudo apt update
-# DM: RKWard also depends on some KDE libraries, not sure which ones, but it works if you add `dolphin breeze-icon-theme oxygen-icon-theme`
-# DM: Might also need qt5ct and QT_QPA_PLATFORMTHEME=qt5ct and gnome-icon-theme
+# DM: Install R and RKWard
 sudo apt install -y r-base r-base-dev rkward oxygen-icon-theme
 
 # Install requirements for packages not part of the CRAN distribution
 sudo apt install -y libgdal-dev libgeos-dev libproj-dev libxml2-dev libcurl4-openssl-dev libssl-dev libudunits2-dev liblwgeom-dev
 
 # PostGIS
-#sudo add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt xenial-pgdg main"
-#sudo apt-key adv --keyserver http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc --recv-keys ACCC4CF8
-#sudo apt update
-sudo apt install -y postgresql-10-postgis-2.4 pgadmin3 postgresql-contrib postgresql-10-postgis-2.4-scripts postgresql-server-dev-10 libpq-dev
+sudo apt install -y postgis pgadmin3 postgresql-contrib postgresql-server-dev-10
 # DM: Create a user and a database with these credentials
 PGUSER="geoscripting"
 export PGPASSWORD="geoscripting"
