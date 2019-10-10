1f605add18899c801006238faae34b81db6e1bad
Dainius Masiliūnas <pastas4@gmail.com>
2019-10-10 15:43:48 +0200

    Update R to at least 3.5
    Older versions are not supported by some packages

--- bionic/install-common.sh
+++ bionic/install-common.sh
@@ -10,6 +10,10 @@ sudo sed -i 's/APT::Periodic::Unattended-Upgrade "1";/APT::Periodic::Unattended-
 sudo apt-key adv --keyserver https://qgis.org/downloads/qgis-2017.gpg.key --recv-keys CAEB3DC3BDF7FB45
 sudo add-apt-repository "https://qgis.org/ubuntu"
 
+# DM: R >3.5
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+
 # DM: Update
 sudo apt update
 sudo apt upgrade -y
