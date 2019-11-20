69853aa8a57fcde063936113d7ee330a77f9ec24
Dainius Masiliūnas <pastas4@gmail.com>
2019-11-20 14:49:57 +0100

    Import key before adding the repository

--- bionic/install-common.sh
+++ bionic/install-common.sh
@@ -11,8 +11,8 @@ sudo apt-key adv --keyserver https://qgis.org/downloads/qgis-2019.gpg.key --recv
 sudo add-apt-repository "https://qgis.org/ubuntu"
 
 # DM: R >3.5
-sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/"
 
 # DM: Update
 sudo apt update
