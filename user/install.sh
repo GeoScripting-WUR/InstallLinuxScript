de916021b191f95ba94f6aaa004308969e59be8f
Dainius Masiliūnas <pastas4@gmail.com>
2019-11-24 15:40:44 +0100

    Update conda packages for 2020

--- user/install.sh
+++ user/install.sh
@@ -4,18 +4,15 @@ bash install-conda.sh
 export PATH="$HOME/miniconda3/bin:$PATH"
 
 # Make a conda environment with all needed tools
-conda create -y --name geoscripting python=3.6 spyder pip jupyter seaborn owslib "poppler<0.62"
+conda create -n geoscripting python=3 pip jupyter seaborn pyproj spyder rasterio geopandas kealib matplotlib owslib pysal descartes -y
 source activate geoscripting
-conda install -y --channel conda-forge folium matplotlib geopy osmnx rasterio geopandas rasterstats pysal descartes \
-  twython nbgrader
-# Fix breakage from https://github.com/conda-forge/rasterio-feedstock/issues/98
-conda install -y --channel conda-forge --override-channels "gdal>2.2.4"
-conda install kealib=1.4.7 -y
+conda install -c conda-forge rasterstats folium geopy osmnx -y
+pip install fiona==1.8.8
 
 # Install IRKernel for Jupyter
 echo 'UserLib=Sys.getenv("R_LIBS_USER"); dir.create(UserLib, recursive=TRUE); install.packages("IRkernel", lib=UserLib, repos="https://cloud.r-project.org"); library(IRkernel, lib=UserLib); IRkernel::installspec()' | R --vanilla
 
 # Activate nbgrader
-jupyter nbextension install --sys-prefix --py nbgrader --overwrite
-jupyter nbextension enable --sys-prefix --py nbgrader
-jupyter serverextension enable --sys-prefix --py nbgrader
+#jupyter nbextension install --sys-prefix --py nbgrader --overwrite
+#jupyter nbextension enable --sys-prefix --py nbgrader
+#jupyter serverextension enable --sys-prefix --py nbgrader
