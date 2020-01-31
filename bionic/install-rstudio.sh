aef834c186dadfe13e57b95da955237776ddc769
Dainius <pastas4@gmail.com>
2020-01-31 19:48:53 +0100

    Update RStudio filename

--- bionic/install-rstudio.sh
+++ bionic/install-rstudio.sh
@@ -3,8 +3,9 @@
 # For bionic specifically, for xenial see ../common/install-rstudio.sh
 
 RSTUDIO_VERSION=${1:-"1.2.5033"}
+DEBFILE=rstudio-${RSTUDIO_VERSION}-amd64.deb
 pushd /tmp
-wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-${RSTUDIO_VERSION}-amd64.deb
-sudo gdebi -n rstudio-xenial-${RSTUDIO_VERSION}-amd64.deb
-rm rstudio-xenial-${RSTUDIO_VERSION}-amd64.deb
+wget https://download1.rstudio.org/desktop/bionic/amd64/${DEBFILE}
+sudo gdebi -n ${DEBFILE}
+rm ${DEBFILE}
 popd
