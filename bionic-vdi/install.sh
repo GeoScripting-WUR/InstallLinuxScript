445db7bd3443270ece2a065cc303a0fd5ffda2b4
Dainius <pastas4@gmail.com>
2020-12-03 20:22:51 +0100

    Update shutdown inhibitor settings

--- bionic-vdi/install.sh
+++ bionic-vdi/install.sh
@@ -6,12 +6,19 @@
 # DM: VMWare Horizon settings: do not inherit keyboard layouts
 #sudo sed -i "s/#KeyboardLayoutSync=FALSE/KeyboardLayoutSync=FALSE/" /etc/vmware/viewagent-custom.conf
 # DM: Set to use GNOME Flashback
-sudo sed -i "s/#UseGnomeFlashback=TRUE/UseGnomeFlashback=TRUE/" /etc/vmware/viewagent-custom.conf
-echo "SSODesktopType=UseGnomeFlashback" | sudo tee -a /etc/vmware/viewagent-custom.conf
+#sudo sed -i "s/#UseGnomeFlashback=TRUE/UseGnomeFlashback=TRUE/" /etc/vmware/viewagent-custom.conf
+#echo "SSODesktopType=UseGnomeFlashback" | sudo tee -a /etc/vmware/viewagent-custom.conf
 # DM: Set sudo timeout to an hour
 sudo sed -i "s/Defaults\tenv_reset/Defaults\tenv_reset,timestamp_timeout=60/" /etc/sudoers
 # DM: Disable prompt to upgrade to a newer version, that would be disastrous for current VMWare version
 sudo sed -i "s/Prompt=.*/Prompt=never/" /etc/update-manager/release-upgrades
+
+# DM: Disable shutdown button
+mkdir -p src
+wget https://github.com/mmartinortiz/RmPwOffBtn/raw/keeping-shutdown-object/src/extension.js -O src/extension.js
+wget https://github.com/mmartinortiz/RmPwOffBtn/raw/keeping-shutdown-object/src/metadata.json -O src/metadata.json
+wget https://github.com/mmartinortiz/RmPwOffBtn/raw/keeping-shutdown-object/install.sh -O install-shutdowninhibitor.sh
+sudo bash ./install-shutdowninhibitor.sh system
 
 # DM: GNOME Flashback options instead, based on dconf
 sudo cp user /etc/dconf/profile/
