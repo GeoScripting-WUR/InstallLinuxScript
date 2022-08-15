fcd5f67a89b9faac21fc417e6c69adc267590877
Dainius Masiliunas <pastas4@gmail.com>
2022-08-15 14:46:01 +0200

    Install ansible-galaxy via Ansible as well

--- focal/install-common.sh
+++ focal/install-common.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt install -y ansible
-ansible-galaxy collection install community.general community.postgresql
+#ansible-galaxy collection install community.general community.postgresql
+ansible-playbook -K --connection=local -i 127.0.0.1, ansible-galaxy.yml
 ansible-playbook -K --connection=local -i 127.0.0.1, geoscripting.yml
