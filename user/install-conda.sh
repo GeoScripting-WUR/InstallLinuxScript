cbcf8dbcdd67752dd8e51a59146c5c2bf3c492fc
Dainius Masiliūnas <pastas4@gmail.com>
2024-10-31 16:10:21 +0100

    Switch from Mambaforge to Miniforge

--- user/install-conda.sh
+++ user/install-conda.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-MINICONDA_VERSION="Mambaforge-Linux-x86_64"
+MINICONDA_VERSION="Miniforge-Linux-x86_64"
 pushd /tmp
-wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
+wget https://github.com/conda-forge/miniforge/releases/latest/download/${MINICONDA_VERSION}.sh
 ## Unattended install, will put into ~/mamba
 bash ${MINICONDA_VERSION}.sh -b -p $HOME/mamba
 rm ${MINICONDA_VERSION}.sh
